57bfbb3a350507f25c734ef4dfd3279b86b7b0c5
yashG <yrg.creation.ai@gmail.com>
2025-11-07 21:52:15 +0530

    gsap/ tailwind initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>App
+      <h1 className="text-3xl text-red-500">hello from TAILWIND</h1>
+    </div>
   )
 }
 
