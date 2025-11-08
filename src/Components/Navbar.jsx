d12243d5d0ebd9c07f94ab4d3d8c00c88b5f58f0
yashG <yrg.creation.ai@gmail.com>
2025-11-08 22:40:20 +0530

    navbar done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 import { navLinks } from '../../constants/index.js'
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react'
 
 const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'nav',
+        start: 'bottom top'
+      }
+    });
+     
+    navTween.fromTo('nav', {backgroundColor: 'transparent'}, {
+      backgroundColor: '#00000050',
+      backgroundFilter: 'blur(10px)',
+      duration: 1,
+      ease: 'power1.inOut'
+    });
+  })
+
+
   return (
     <nav className=''>
       <div className="nav-hold flex items-center justify-between py-4 px-8">
