25a9ca61c5228f52b41715fafc41a6a55c70554c
yashG <yrg.creation.ai@gmail.com>
2025-11-08 22:30:36 +0530

    new js file for constants

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import { navLinks } from '../../constants/index.js'
 
 const Navbar = () => {
   return (
     <nav className=''>
       <div className="nav-hold flex items-center justify-between py-4 px-8">
-        <a href="#home" className= "">
-          <span className="text-2xl font-bold">YMojito</span>
+        <a href="#home" className= "flex gap-2">
+          <img src="/images/logo.png" alt="Logo" />
+          <span className="text-2xl font-bold">YMojito's</span>
         </a>
+
+        <ul className='flex items-center justify-between gap-8 list-none'>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+               <a href= {`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+
       </div>
     </nav>
   )
