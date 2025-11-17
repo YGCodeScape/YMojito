aff3168f589b19970b083def0a75a1f9fc6bcd2a
yashG <yrg.creation.ai@gmail.com>
2025-11-17 19:36:34 +0530

    working on art section

---
+++ src/Components/Art.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Art = () => {
+  return (
+    <div id="Art" className='min-h-dvh radial-gradient'>
+        <div className="container mx-auto h-full pt-20 ">
+            <h2 className="will-fade text-center">The ART</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Art
