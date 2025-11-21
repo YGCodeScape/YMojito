f94a510fbef7ff3e7d8fa9e8f6dc457be89606c6
yashG <yrg.creation.ai@gmail.com>
2025-11-22 02:48:35 +0530

    footer section aded

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,26 +1,57 @@
 import React from 'react'
-import { openingHours } from '../../constants'
+import { openingHours, socials } from '../../constants'
+import { useGSAP } from '@gsap/react'
+import { SplitText } from 'gsap/all'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 const Contact = () => {
+
+    useGSAP(() => {
+        const titleSplit = SplitText.create('#contact h2', {
+            type: 'words'
+        });
+
+        const timeline = gsap.timeline({
+            scrollTrigger:  {
+                trigger: '#contact',
+                start: 'top center',
+            },
+            ease: "power1.inOut"
+        })
+
+        timeline.from(titleSplit.words, {
+            opacity: 0,
+            yPercent: 100,
+            stagger: 0.2
+        })
+        .from('#contact h3, #contact p', {
+            opacity: 0,
+            yPercent: 100,
+            stagger: 0.02
+        })
+    })
+
   return (
-    <footer id='contact'>
+    <div id="contact" className="radial-gradient">
         <img src="/images/footer-right-leaf.png" alt="" id="f-right-leaf"/>
         <img src="/images/footer-left-leaf.png" alt="" id="f-left-leaf"/>
 
-        <div className="f-content">
+        <div className="f-content container relative ">
              <h2>Where to Find Us</h2>
 
-             <div className="f-row1">
+             <div>
                 <h3>Visit Our Bar</h3>
                 <p>345, Ret, Blod, $4033, London CA 60604</p>
              </div>
 
-             <div className="f-row1">
+             <div>
                 <h3>Contact Us</h3>
                 <p>(555) 754-3452</p>
+                <p>contactymojito@gmil.com</p>
              </div>
 
-             <div className="f-col3">
+             <div>
                  <h3>Open Every Day</h3>
                  {openingHours.map((time) => (
                     <p key={time.day}>
@@ -28,9 +59,17 @@ const Contact = () => {
                     </p>
                  ))}
              </div>
-
+            
+              <h5>Socials</h5>
+             <div className="social">
+                 {socials.map((social) => (
+                    <a className="" key={social.name} href={social.url} target="._blank" rel="noopener noreferrer">
+                        <img src={social.icon} alt="" />
+                    </a>
+                 ))}
+             </div>
         </div>
-    </footer>
+    </div>
   )
 }
 
